7aaf8d78226a484a0409e702060f04a182dd01e7
daijinming <daijinming@live.cn>
2017-01-05 17:12:11 +0800

    dynatable 演示

---
+++ NancyDemo.Dynatable/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NancyDemo.Dynatable.Startup))]
+namespace NancyDemo.Dynatable
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+                        
+            app.UseNancy();
+        }
+    }
+}
